9c2d7c2ef9b451d320cbe0753e3aa78dab6c8d0f
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-10-14 22:19:00 +0900

    Added name to User table in README.md

--- db/migrate/20171013071626_add_profile_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddProfileToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :profile, :string
-    add_column :users, :works, :string
-    add_column :users, :avatar, :string
-  end
-end
