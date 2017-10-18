a5d5a9ba4539d0a25c65fc18f00ad5f90b05b817
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-10-18 16:16:46 +0900

    Set update method and user_params which is as a callback method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,17 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    User.update(user_params)
+    redirect_to(user_path)
+  end
+
   private
   def set_user
     @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :occupation, :profile, :works)
   end
 end
