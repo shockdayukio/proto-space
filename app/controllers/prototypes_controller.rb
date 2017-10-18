ce7ffdea35f531685b8d7b47012572116fe52dee
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-10-18 16:10:03 +0900

    Set set_user as a callback method in prototypes controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
 
+before_action :set_user, only: [:index, :show]
+
   def index
   end
 
@@ -7,5 +9,10 @@ class PrototypesController < ApplicationController
   end
 
   def new
+  end
+
+  private
+  def set_user
+    @user = current_user
   end
 end
