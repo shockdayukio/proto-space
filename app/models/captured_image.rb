7fa149f8189167c324dec1d59a00a7da449ab8f6
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-04 22:56:46 +0900

    Set an inverse_of option in belongs_to association, enum method  to tell a main image from sub one and a validation to the status column in the capturedimage model

--- app/models/captured_image.rb
+++ app/models/captured_image.rb
@@ -1,4 +1,14 @@
 class CapturedImage < ApplicationRecord
-  belongs_to :prototype
+  belongs_to :prototype, inverse_of: :captured_images
+  enum status: { main: 0, sub: 1 }
+
   mount_uploader :image, ImageUploader
+
+  validate :only_one_main_image_per_prototype
+
+  def only_one_main_image_per_prototype
+    main_image = self.prototype.captured_images.main
+    errors.add(:status, "Main Image for this Prototype has already exists.") if main_image.present?
+  end
 end
+
