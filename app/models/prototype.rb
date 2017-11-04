12bd0e1f8801a0fedf438aa07158200c50d48def
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-04 23:00:49 +0900

    Set an inverse_of option in has_many association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ApplicationRecord
   belongs_to :user
-  has_many :captured_images
+  has_many :captured_images, inverse_of: :prototype
   accepts_nested_attributes_for :captured_images
 end
