eb73b458578edbcd37ff0747dcd7a75c9812f285
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-12 19:10:31 +0900

    Set validations to name, catch_copy and concept column

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,8 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_many :captured_images, inverse_of: :prototype
   accepts_nested_attributes_for :captured_images
+
+  validates :name, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
 end
