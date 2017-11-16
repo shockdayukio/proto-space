b0fbd5b708a572ddf9513772215a450e48fb4ea1
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-16 11:48:53 +0900

    Update not to validate captured_images in posting a prototype

--- app/models/captured_image.rb
+++ app/models/captured_image.rb
@@ -3,12 +3,4 @@ class CapturedImage < ApplicationRecord
   enum status: { main: 0, sub: 1 }
 
   mount_uploader :image, ImageUploader
-
-  validate :only_one_main_image_per_prototype
-
-  def only_one_main_image_per_prototype
-    main_image = self.prototype.captured_images.main
-    errors.add(:status, "Main Image for this Prototype has already exists.") if main_image.present?
-  end
 end
-
