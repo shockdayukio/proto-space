1dfea1bad3d9b51283a66bb12da87ddbb98c0592
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-19 01:18:16 +0900

    To enable to delete a prototype

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
-  has_many :captured_images, inverse_of: :prototype
+  has_many :captured_images, inverse_of: :prototype, dependent: :destroy
   accepts_nested_attributes_for :captured_images
 
   validates :name, :catch_copy, :concept ,presence: true
