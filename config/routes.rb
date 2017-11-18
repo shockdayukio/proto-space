b1fdb0a52d8a75b8f35bae46e3c5a1893dfc2297
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-19 00:27:07 +0900

    Set routes to a edit and destroy action in prototypes controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :prototypes, only: [:index, :show, :new, :create]
+  resources :prototypes, only: [:index, :show, :new, :create, :destroy, :update]
 end
