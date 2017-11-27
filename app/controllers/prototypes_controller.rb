a08858c01408c3d482133994c60dffaae383eb06
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-28 01:52:27 +0900

    Set a find_prototype method to shorten codes

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,16 +1,14 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :find_prototype, only: [:show, :edit, :update]
+
 
   def index
     @prototypes = Prototype.all
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
-    @user = @prototype.user
-    @main_image = @prototype.captured_images.main
-    @sub_images = @prototype.captured_images.sub
   end
 
   def new
@@ -29,13 +27,9 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
-    @main_images = @prototype.captured_images.main
-    @sub_images = @prototype.captured_images.sub
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototypes_path, notice: "Succeeded to update your prototype"
     else
@@ -44,7 +38,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    if Prototype.destroy(delete_params[:id])
+    if Prototype.destroy(params[:id])
       redirect_to prototypes_path, notice: "Succeeded to delete your prototype"
     else
       redirect_to prototypes_path, alert: "Failed to delete your prototype"
@@ -56,7 +50,7 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:name, :catch_copy, :concept, :user_id, captured_images_attributes: [:id, :image, :status])
   end
 
-  def delete_params
-    params.permit(:id)
+  def find_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
