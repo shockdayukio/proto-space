fdb7f3b0df00ec8fcbb5515d2ee30a2f6ebd807b
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-11-28 01:58:38 +0900

    Set a main_image method and a sub_images method to shorten cords in the views

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,4 +4,12 @@ class Prototype < ApplicationRecord
   accepts_nested_attributes_for :captured_images
 
   validates :name, :catch_copy, :concept ,presence: true
+
+  def main_image
+    self.captured_images.main.first
+  end
+
+  def sub_images
+    self.captured_images.sub
+  end
 end
