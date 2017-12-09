d822b0d5d925cc66a0e2b68f6a9985501b79afbc
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-09 21:54:47 +0900

    Implemented a like function in partial templates via ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,32 @@
 class LikesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:create, :destroy]
+
   def create
-    current_user.likes.create(like_params)
+    current_user.likes.create(prototype_id: like_params[:id])
+    set_prototype
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
-    Like.destroy
+    current_user.likes.find_by(prototype_id: like_params[:id]).destroy
+    set_prototype
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
 
   def like_params
-    params.permit(:prototype_id)
+    params.permit(:id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
