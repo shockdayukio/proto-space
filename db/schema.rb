dc59446bdefd92eb7292b854084eeb2b286873ba
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-09 21:49:39 +0900

    Added an unique multiple index on the columns of the prototype_id and the user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207075052) do
+ActiveRecord::Schema.define(version: 20171209121553) do
 
   create_table "captured_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "image",        default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171207075052) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["prototype_id", "user_id"], name: "index_likes_on_prototype_id_and_user_id", unique: true, using: :btree
     t.index ["prototype_id"], name: "index_likes_on_prototype_id", using: :btree
     t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
