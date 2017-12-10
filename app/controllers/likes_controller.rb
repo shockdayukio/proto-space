f57dfc8b2036fcc31cc1fc0ae89196e91c8098e3
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-10 13:25:26 +0900

    Reduced useless codes and fixed a method's name

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,26 +3,24 @@ class LikesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    current_user.likes.create(prototype_id: like_params[:id])
+    current_user.likes.create(prototype_id: prototype_params[:id])
     set_prototype
     respond_to do |format|
-      format.html
       format.js
     end
   end
 
   def destroy
-    current_user.likes.find_by(prototype_id: like_params[:id]).destroy
+    current_user.likes.find_by(prototype_id: prototype_params[:id]).destroy
     set_prototype
     respond_to do |format|
-      format.html
       format.js
     end
   end
 
   private
 
-  def like_params
+  def prototype_params
     params.permit(:id)
   end
 
