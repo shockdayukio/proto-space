2c15bb93caf0ccbf11e7cca259a5657dad514e7d
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-12 00:40:58 +0900

    Create a comment and set associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :prototypes
   has_many :likes
+  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
