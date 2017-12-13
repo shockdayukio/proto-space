106728c7a578c5be91358f034e9ccb9da63d4a19
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-13 22:00:57 +0900

    Set routes to use namespace

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,4 +1,4 @@
-class NewestController < ApplicationController
+class Prototypes::NewestController < ApplicationController
   def index
     @prototypes = Prototype.includes(:user).created_at.page(params[:page]).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES)
   end
