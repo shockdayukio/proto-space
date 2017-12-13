f349a79c9bec0e11cbf77efc783cd9b4e55731cc
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-13 22:00:57 +0900

    Set routes to use namespace

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,4 +1,4 @@
-class PopularController < ApplicationController
+class Prototypes::PopularController < ApplicationController
   def index
     @prototypes = Prototype.includes(:user).like_count.page(params[:page]).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES)
   end
