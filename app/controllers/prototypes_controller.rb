78e400441d92dd19c1e4f43dd5ee7293bb731266
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-17 02:29:40 +0900

    Fixed an argument of an includes method to slove N+1 problem

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,7 +4,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.includes(:user, :taggings).page(params[:page]).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES)
+    @prototypes = Prototype.includes(:user, :tags).page(params[:page]).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES)
   end
 
   def show
