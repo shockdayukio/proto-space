508f7f45a743cf9b80215ed8cfc12687f972a9e5
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-17 19:49:02 +0900

    Fixed routes to the tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,13 +5,13 @@ class TagsController < ApplicationController
   end
 
   def show
-    @tag = ActsAsTaggableOn::Tag.find(tag_params[:id])
+    @tag = ActsAsTaggableOn::Tag.find_by(name: tag_params[:name])
     @prototypes_count = Prototype.tagged_with(@tag).length
     @prototypes = Prototype.tagged_with(@tag.name).includes(:user, :tags).page(@tag.name).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES)
   end
 
   private
   def tag_params
-    params.permit(:id)
+    params.permit(:name)
   end
 end
