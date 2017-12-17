aa61423d8081f6b2a4b9788d75b1ae008659ba5a
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-17 19:06:14 +0900

    Deleted the tag method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,11 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = tag.most_used
+    @tags = ActsAsTaggableOn::Tag.most_used
   end
 
   def show
-    @tag = tag.find(tag_params[:id])
+    @tag = ActsAsTaggableOn::Tag.find(tag_params[:id])
     @prototypes_count = Prototype.tagged_with(@tag).length
     @prototypes = Prototype.tagged_with(@tag.name).includes(:user, :tags).page(@tag.name).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES)
   end
@@ -13,9 +13,5 @@ class TagsController < ApplicationController
   private
   def tag_params
     params.permit(:id)
-  end
-
-  def tag
-    ActsAsTaggableOn::Tag
   end
 end
