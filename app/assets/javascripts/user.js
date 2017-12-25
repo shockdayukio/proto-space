0a9d9ae3361dd1fb5165621b35d0f74f6c1d1a9c
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-25 14:15:11 +0900

    Refactored javascript files to delete useless functions

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,8 @@
 $(function(){
   $('#js-avatar-input').change(function(e){
-    var file = this.files[0],
-        img = $(this).siblings('img'),
-        fileReader = new FileReader();
+    var file = this.files[0];
+    var img = $(this).siblings('img');
+    var fileReader = new FileReader();
 
     fileReader.onload = function(event) {
         img.attr({"src": event.target.result});
