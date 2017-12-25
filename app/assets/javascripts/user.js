68e1930215f11de8a8339544b4e50aeef1c3c17e
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-26 00:53:12 +0900

    Refactored to rename a constant and not to use a magic number

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,6 @@
 $(function(){
   $('#js-avatar-input').change(function(e){
-    var file = this.files[INDEX_OF_IMAGE_FILE_IN_JQUERY_OBJECT];
+    var file = this.files[INDEX_OF_IMAGE_FILE];
     var img = $(this).siblings('img');
     var fileReader = new FileReader();
 
