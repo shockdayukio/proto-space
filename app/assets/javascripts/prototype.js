0036eafba5135d6a788178382061af4deb5ff56d
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-26 15:59:52 +0900

    Renamed a variable to be clearly

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -5,7 +5,7 @@ function appendSubImageForm(sub_image_id){
   var html = '<li class="sub-image-item col-md-4">'+
                '<div class="image-upload">' +
                  '<img>' +
-                 '<input type="file" name="prototype[captured_images_attributes][' + sub_image_id + '][image]" id="prototype_captured_images_attributes_' + sub_image_id + '_image" class="js-sub-image-file js-image">' +
+                 '<input type="file" name="prototype[captured_images_attributes][' + sub_image_id + '][image]" id="prototype_captured_images_attributes_' + sub_image_id + '_image" class="js-image-file js-image">' +
                  '<input type="hidden" name="prototype[captured_images_attributes][' + sub_image_id + '][status]" id="prototype_captured_images_attributes_' + sub_image_id + '_status" value="sub">' +
                '</div>' +
              '</li>';
@@ -21,7 +21,7 @@ function appendSubImageFormButton(){
   $("#js-proto-sub-list").append(html);
 };
 
-$(document).on('change', '.js-sub-image-file', function(){
+$(document).on('change', '.js-image-file', function(){
   var file = this.files[INDEX_OF_IMAGE_FILE];
   var image = $(this).siblings('img');
   var fileReader = new FileReader();
@@ -35,12 +35,13 @@ $(document).on('change', '.js-sub-image-file', function(){
 });
 
 $(document).on('click', '#js-image-plus', function(){
-  var group_items_count = $(".sub-image-item").length;
+  var sub_image_item_count = $(".sub-image-item").length;
   $(this).remove();
-  if (group_items_count <= MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE){
-    var sub_image_id = group_items_count;
+  if (sub_image_item_count <= MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE){
+    var sub_image_id = $(".js-image-file").length;
+    console.log(sub_image_id);
     appendSubImageForm(sub_image_id);
-    if (group_items_count < MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE){
+    if (sub_image_item_count < MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE){
       appendSubImageFormButton();
     }
   };
