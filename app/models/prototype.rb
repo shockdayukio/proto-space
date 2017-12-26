a6de00ff54eee1f39253e0b6d04fdda1be354314
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-26 14:00:46 +0900

    Fixed the prototype model to set constants and methods first

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,15 +1,9 @@
 class Prototype < ApplicationRecord
-  belongs_to :user
-  has_many :captured_images, inverse_of: :prototype, dependent: :destroy
-  accepts_nested_attributes_for :captured_images
-  has_many :likes
-  has_many :comments
 
-  acts_as_taggable
-
-  validates :name, :catch_copy, :concept ,presence: true
-  validate :no_more_than_three_tags
-  validate :no_more_than_six_sub_images
+  NUMBER_OF_DISPLAYED_PROTOTYPES = 8
+  NUMBER_OF_SUB_IMAGES_IN_NEW_PROTOTYPE_FORM = 2
+  MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE = 6
+  NUMBER_OF_TAGS_IN_A_PROTOTYPE = 3
 
   def no_more_than_three_tags
     if self.tag_list.count > NUMBER_OF_TAGS_IN_A_PROTOTYPE
@@ -22,14 +16,6 @@ class Prototype < ApplicationRecord
       errors.add('sub_images', "cannot be contained more than #{MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE}")
     end
   end
-
-  scope :newest_order, -> { order("created_at DESC") }
-  scope :from_highest_count, -> { order("like_count DESC") }
-
-  NUMBER_OF_DISPLAYED_PROTOTYPES = 8
-  NUMBER_OF_SUB_IMAGES_IN_NEW_PROTOTYPE_FORM = 2
-  MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE = 6
-  NUMBER_OF_TAGS_IN_A_PROTOTYPE = 3
 
   def main_image
     self.captured_images.main.present? ? self.captured_images.main.first.image : 'no-image.png'
@@ -55,4 +41,20 @@ class Prototype < ApplicationRecord
   def created_at
     self['created_at'].to_s(:date)
   end
+
+  belongs_to :user
+  has_many :captured_images, inverse_of: :prototype, dependent: :destroy
+  accepts_nested_attributes_for :captured_images
+  has_many :likes
+  has_many :comments
+
+  acts_as_taggable
+
+  validates :name, :catch_copy, :concept ,presence: true
+  validate :no_more_than_three_tags
+  validate :no_more_than_six_sub_images
+
+  scope :newest_order, -> { order("created_at DESC") }
+  scope :from_highest_count, -> { order("like_count DESC") }
+
 end
