256d350533bbe9f5d54a735dec95924107a674b6
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-26 12:34:13 +0900

    Deleted a function which is useless

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,12 @@
-$(function(){
-  $('#js-avatar-input').change(function(e){
-    var file = this.files[INDEX_OF_IMAGE_FILE];
-    var img = $(this).siblings('img');
-    var fileReader = new FileReader();
+$('#js-avatar-input').change(function(e){
+  var file = this.files[INDEX_OF_IMAGE_FILE];
+  var img = $(this).siblings('img');
+  var fileReader = new FileReader();
 
-    fileReader.onload = function(event) {
-        img.attr({"src": event.target.result});
-    };
+  fileReader.onload = function(event) {
+      img.attr({"src": event.target.result});
+  };
 
-    fileReader.readAsDataURL(file);
-  });
+  fileReader.readAsDataURL(file);
 });
+
