6d0fe21e872c144486d56ff2528c7f8b985e8073
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-12-26 16:29:09 +0900

    Renamed a variable from sub_image_id to next_sub_image_id to be clear

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,12 +1,12 @@
 const MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE = 6;
 const INDEX_OF_IMAGE_FILE = 0;
 
-function appendSubImageForm(sub_image_id){
+function appendSubImageForm(next_sub_image_id){
   var html = '<li class="sub-image-item col-md-4">'+
                '<div class="image-upload">' +
                  '<img>' +
-                 '<input type="file" name="prototype[captured_images_attributes][' + sub_image_id + '][image]" id="prototype_captured_images_attributes_' + sub_image_id + '_image" class="js-image-file js-image">' +
-                 '<input type="hidden" name="prototype[captured_images_attributes][' + sub_image_id + '][status]" id="prototype_captured_images_attributes_' + sub_image_id + '_status" value="sub">' +
+                 '<input type="file" name="prototype[captured_images_attributes][' + next_sub_image_id + '][image]" id="prototype_captured_images_attributes_' + next_sub_image_id + '_image" class="js-image-file js-image">' +
+                 '<input type="hidden" name="prototype[captured_images_attributes][' + next_sub_image_id + '][status]" id="prototype_captured_images_attributes_' + next_sub_image_id + '_status" value="sub">' +
                '</div>' +
              '</li>';
   $('#js-proto-sub-list').append(html);
@@ -38,8 +38,8 @@ $(document).on('click', '#js-image-plus', function(){
   var sub_image_item_count = $(".sub-image-item").length;
   $('#js-image-plus').remove();
   if (sub_image_item_count <= MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE){
-    var sub_image_id = $(".js-image-file").length;
-    appendSubImageForm(sub_image_id);
+    var next_sub_image_id = $(".js-image-file").length;
+    appendSubImageForm(next_sub_image_id);
     if (sub_image_item_count < MAXIMUM_NUMBER_OF_SUB_IMAGES_IN_PROTOTYPE){
       appendSubImageFormButton();
     }
