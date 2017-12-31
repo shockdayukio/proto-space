ad880f9078bdb6a7ee1556f89b1b118f1874872d
Yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-01 05:31:12 +0900

    Set config of the rspec to test with gems rails-controller-testing and devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require 'devise'
+require 'support/controller_macros.rb'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -12,4 +15,26 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+
+  # To test controller with the gem 'rails-controller-testing'
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
+  end
+
+
+  # To test controllers with the devise
+  config.include Devise::TestHelpers, type: :controller
+  # use Devise::TestHelpers on the form of an instance method
+  config.include ControllerMacros, type: :controller
+  # use Devise::TestHelpers on the form of an class method
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
