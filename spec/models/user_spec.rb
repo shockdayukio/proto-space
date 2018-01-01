37ad815d24000e563f7c0e6db9a9f3a6cf843810
Yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-01 16:17:29 +0900

    Updated test for the user model to use let method and add tests validation on password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,60 +1,86 @@
 require 'rails_helper'
 
 describe 'User' do
-  describe '#create' do
-    it "is valid with necessary information" do
-      user = create(:user)
-      expect(user).to eq User.last
+  let(:user){ create(:user)}
+
+  describe 'validation' do
+    context 'when a user is filled all valid attributes' do
+      it "is valid" do
+        expect(user).to be_valid
+      end
     end
 
-    it "is invalid without an email" do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+    context 'when a user lacks of one valid attributes' do
+      it "is invalid without an email" do
+        user.email = nil
+        user.valid?
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+
+      it "is invalid without a password" do
+        user.password = nil
+        user.valid?
+        expect(user.errors[:password]).to include("can't be blank")
+      end
+
+      it "is invalid without a name" do
+        user.name = nil
+        user.valid?
+        expect(user.errors[:name]).to include("can't be blank")
+      end
+
+      it "is invalid without a profile" do
+        user.profile = nil
+        user.valid?
+        expect(user.errors[:profile]).to include("can't be blank")
+      end
+
+      it "is invalid without a works" do
+        user.works = nil
+        user.valid?
+        expect(user.errors[:works]).to include("can't be blank")
+      end
+
+      it "is invalid without an occupation" do
+        user.occupation = nil
+        user.valid?
+        expect(user.errors[:occupation]).to include("can't be blank")
+      end
     end
 
-    it "is invalid with a email that has already been saved" do
-      user = create(:user)
-      second_user = build(:user, email: user.email)
-      second_user.valid?
-      expect(second_user.errors[:email]).to include("has already been taken")
+    context 'when a user contains one of attributes that has already been saved' do
+      it "is invalid with a email that has already been saved" do
+        second_user = build(:user, email: user.email)
+        second_user.valid?
+        expect(second_user.errors[:email]).to include("has already been taken")
+      end
+
+      it "is invalid with a name that has already been saved" do
+        second_user = build(:user, name: user.name)
+        second_user.valid?
+        expect(second_user.errors[:name]).to include("has already been taken")
+      end
     end
 
-    it "is invalid without a password" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
-    end
+    context 'when checking the length of the password' do
+      it "is valid if an user's password is within 6 to 128 characters" do
+        user.password = Faker::Internet.password(6, 128, true, true)
+        user.password_confirmation = user.password
+        user.valid?
+        expect(user).to be_valid
+      end
 
-    it "is invalid without a name" do
-      user = build(:user, name: nil)
-      user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
-    end
+      it "is valid if an user's password is less than 6 characters" do
+        user.password = Faker::Internet.password(1, 5, true, true)
+        user.valid?
+        expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+      end
 
-    it "is invalid with a name that has already been saved" do
-      user = create(:user)
-      second_user = build(:user, name: user.name)
-      second_user.valid?
-      expect(second_user.errors[:name]).to include("has already been taken")
-    end
-
-    it "is invalid without a profile" do
-      user = build(:user, profile: nil)
-      user.valid?
-      expect(user.errors[:profile]).to include("can't be blank")
-    end
-
-    it "is invalid without a works" do
-      user = build(:user, works: nil)
-      user.valid?
-      expect(user.errors[:works]).to include("can't be blank")
-    end
-
-    it "is invalid without an occupation" do
-      user = build(:user, occupation: nil)
-      user.valid?
-      expect(user.errors[:occupation]).to include("can't be blank")
+      it "is valid if an user's password is has more than 129 characters" do
+        user.password = Faker::Internet.password(129)
+        user.valid?
+        expect(user.errors[:password]).to include("is too long (maximum is 128 characters)")
+      end
     end
   end
 end
