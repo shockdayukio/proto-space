38a02ee108f4375bd528c0260901bd247dd8a092
Yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-02 02:12:48 +0900

    Set config to test with database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,12 +6,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
+require 'database_cleaner'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
@@ -40,4 +40,16 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
   # automatically infer spec type from a file location
+
+  # setting for test with database_cleaner
+  config.use_transactional_fixtures = false
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
