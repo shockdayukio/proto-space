dab1eda8f299d224472ce0cb7220fb5556b94681
Yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-02 20:26:34 +0900

    Added tests file for the users controller to confirm when nessesary attributes are no contents

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include ActionDispatch::TestProcess
 
 describe User do
-  let(:user){ create(:user) }
+  let(:user){ build(:user) }
 
   describe 'validation' do
     context 'when a user is filled all valid attributes' do
@@ -12,50 +12,118 @@ describe User do
     end
 
     context 'when a user lacks of one valid attributes' do
-      it "is invalid without an email" do
-        user.email = nil
-        user.valid?
-        expect(user.errors[:email]).to include("can't be blank")
+
+      describe 'email' do
+        it "is invalid with no content" do
+          user.email = ""
+          user.valid?
+          expect(user.errors[:email]).to include("can't be blank")
+        end
+        it "is invalid with nil" do
+          user.email = nil
+          user.valid?
+          expect(user.errors[:email]).to include("can't be blank")
+        end
       end
 
-      it "is invalid without a password" do
-        user.password = nil
-        user.valid?
-        expect(user.errors[:password]).to include("can't be blank")
+      describe "password" do
+        it "is invalid with nil" do
+          user.password = nil
+          user.valid?
+          expect(user.errors[:password]).to include("can't be blank")
+        end
+        describe 'when checking the length of the password' do
+          it "is valid if an user's password is within 6 to 128 characters" do
+            user.password = Faker::Internet.password(6, 128, true, true)
+            user.valid?
+            expect(user).to be_valid
+          end
+
+          it "is valid if an user's password is less than 6 characters" do
+            user.password = Faker::Internet.password(1, 5, true, true)
+            user.valid?
+            expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+          end
+
+          it "is invalid if an user's password is has more than 129 characters" do
+            user.password = Faker::Internet.password(129)
+            user.valid?
+            expect(user.errors[:password]).to include("is too long (maximum is 128 characters)")
+          end
+        end
       end
 
-      it "is invalid without a name" do
-        user.name = nil
-        user.valid?
-        expect(user.errors[:name]).to include("can't be blank")
+      describe 'name' do
+        it "is invalid with no content" do
+          user.name = ""
+          user.valid?
+          expect(user.errors[:name]).to include("can't be blank")
+        end
+        it "is invalid with nil" do
+          user.name = nil
+          user.valid?
+          expect(user.errors[:name]).to include("can't be blank")
+        end
       end
 
-      it "is invalid without a profile" do
-        user.profile = nil
-        user.valid?
-        expect(user.errors[:profile]).to include("can't be blank")
+      describe 'profile' do
+        it "is invalid with no content" do
+          user.profile = ""
+          user.valid?
+          expect(user.errors[:profile]).to include("can't be blank")
+        end
+        it "is invalid with nil" do
+          user.profile = nil
+          user.valid?
+          expect(user.errors[:profile]).to include("can't be blank")
+        end
       end
 
-      it "is invalid without a works" do
-        user.works = nil
-        user.valid?
-        expect(user.errors[:works]).to include("can't be blank")
+      describe "work" do
+        it "is invalid with no content" do
+          user.works = ""
+          user.valid?
+          expect(user.errors[:works]).to include("can't be blank")
+        end
+        it "is invalid with nil" do
+          user.works = nil
+          user.valid?
+          expect(user.errors[:works]).to include("can't be blank")
+        end
       end
 
-      it "is invalid without a avatar" do
-        user.avatar = nil
-        user.valid?
-        expect(user.errors[:avatar]).to include("can't be blank")
+      describe "avatar" do
+        it "invalid with no content" do
+          user.avatar = ""
+          user.valid?
+          expect(user.errors[:avatar]).to include("can't be blank")
+        end
+        it "is invalid with nil" do
+          user.avatar = nil
+          user.valid?
+          expect(user.errors[:avatar]).to include("can't be blank")
+        end
       end
 
-      it "is invalid without an occupation" do
-        user.occupation = nil
-        user.valid?
-        expect(user.errors[:occupation]).to include("can't be blank")
+      describe "occupation" do
+        it "is invalid with no content" do
+          user.occupation = ""
+          user.valid?
+          expect(user.errors[:occupation]).to include("can't be blank")
+        end
+        it "is invalid with nil" do
+          user.occupation = nil
+          user.valid?
+          expect(user.errors[:occupation]).to include("can't be blank")
+        end
       end
     end
 
     context 'when a user contains one of attributes that has already been saved' do
+      before :each do
+        create(:user)
+      end
+
       it "is invalid with a email that has already been saved" do
         second_user = build(:user, email: user.email)
         second_user.valid?
@@ -66,26 +134,6 @@ describe User do
         second_user = build(:user, name: user.name)
         second_user.valid?
         expect(second_user.errors[:name]).to include("has already been taken")
-      end
-    end
-
-    context 'when checking the length of the password' do
-      it "is valid if an user's password is within 6 to 128 characters" do
-        user.password = Faker::Internet.password(6, 128, true, true)
-        user.valid?
-        expect(user).to be_valid
-      end
-
-      it "is valid if an user's password is less than 6 characters" do
-        user.password = Faker::Internet.password(1, 5, true, true)
-        user.valid?
-        expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
-      end
-
-      it "is valid if an user's password is has more than 129 characters" do
-        user.password = Faker::Internet.password(129)
-        user.valid?
-        expect(user.errors[:password]).to include("is too long (maximum is 128 characters)")
       end
     end
   end
