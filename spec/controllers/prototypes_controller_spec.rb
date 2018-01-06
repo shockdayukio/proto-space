2ae52e935ad7bbadc4e4b341c9c6a634d64773c2
Yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-06 22:47:45 +0900

    tmp test on the prototypes controller

---
+++ spec/controllers/prototypes_controller_spec.rb
@@ -0,0 +1,249 @@
+require 'rails_helper'
+include ActionDispatch::TestProcess
+
+describe PrototypesController do
+  let(:prototype)           { create(:prototype, :with_comments) }
+  let(:user)                {prototype.user}
+  let(:prototype_name)       { Faker::Beer.name }
+  let(:other_prototype)     { create(:prototype)}
+
+  let(:valid_params)         { { id: prototype.id, prototype: attributes_for(:prototype, name: prototype_name) } }
+  let(:invalid_params)       { { id: prototype.id, prototype: attributes_for(:prototype, name: nil) } }
+
+
+  describe 'with an user login' do
+    before :each do
+      login_user(user)
+    end
+
+    context 'GET #new' do
+      let(:placeholders)  { %W(Web\sdesign UI Application) }
+      let(:new_prototype) { build(:new_prototype) }
+
+      before :each do
+        get :new
+      end
+
+      it "assigns an array of tag examples to @placeholders" do
+        expect(assigns(:placeholders)).to eq placeholders
+      end
+
+      it "assigns a prototype instance to @prototype" do
+        expect(assigns(:prototype)).to be_a_new(Prototype)
+      end
+
+      it "renders :new template" do
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'POST #create' do
+
+      context '@prototype can be saved' do
+        before :each do
+          post :create, params: valid_params
+        end
+
+        it "assigns a prototype instance which the current user will create to @prototype" do
+          # expect(assigns(:prototype)).to be_a_new(Prototype)
+        end
+
+        it "redirect to :index template" do
+          expect(response).to redirect_to prototypes_path
+        end
+
+        it "adds a flash message" do
+          expect(flash[:notice]).to include "Succeeded to publish your prototype"
+        end
+      end
+
+      context '@prototype can\'t be saved' do
+        before :each do
+          post :create, params: invalid_params
+        end
+
+        it "assigns a prototype instance which the current user will create to @prototype" do
+          expect(assigns(:prototype)).to be_a_new(Prototype)
+        end
+
+        it "redirect to :new template" do
+          expect(response).to redirect_to new_prototype_path
+        end
+
+        it "adds a flash message" do
+          expect(flash[:alert]).to eq "Failed to publish your prototype"
+        end
+      end
+    end
+
+    context 'GET #edit' do
+      before :each do
+        get :edit, params: { id: prototype.id }
+      end
+
+      it "assigns the requested prototype to @prototype" do
+        expect(assigns(:prototype)).to eq prototype
+      end
+
+      it "renders :edit template" do
+        expect(response).to render_template :edit
+      end
+    end
+
+    context 'PATCH #update' do
+      context '@prototype can be saved' do
+        before :each do
+          patch :update, params: valid_params
+        end
+
+        it "redirect to :index template" do
+          expect(response).to redirect_to prototypes_path
+        end
+
+        it "adds a flash message" do
+          expect(flash[:notice]).to eq "Succeeded to update your prototype"
+        end
+      end
+
+      context '@prototype can\'t be saved' do
+        before :each do
+          patch :update, params: invalid_params
+        end
+
+        it "redirect to :new template" do
+          expect(response).to redirect_to edit_prototype_path
+        end
+
+        it "adds a flash message" do
+          expect(flash[:alert]).to eq "Failed to update your prototype"
+        end
+      end
+    end
+
+    context 'DELETE #destroy' do
+      context 'when the user created the prototype' do
+        before :each do
+          delete :destroy, params: { id: prototype.id }
+        end
+        context '@prototype can be destroyed' do
+          it "is redirect to :index template" do
+            expect(response).to redirect_to prototypes_path
+          end
+
+          it "adds a flash message" do
+            expect(flash[:notice]).to eq "Succeeded to delete your prototype"
+          end
+        end
+
+        context '@prototype can\'t be destroyed' do
+          it "is redirect to :index template" do
+            expect(response).to redirect_to prototypes_path
+          end
+
+          it "adds a flash message" do
+            expect(flash[:alert]).to eq "Failed to delete your prototype"
+          end
+        end
+      end
+
+      context 'when the user did\'t create the prototype' do
+        before :each do
+          delete :destroy, params: { id: other_prototype.id }
+        end
+
+        it 'redirect to :index template' do
+          expect(response).to redirect_to prototypes_path
+        end
+
+        it 'adds a flash message' do
+          expect(flash[:alert]).to eq "Cannot delete other's prototype"
+        end
+      end
+    end
+  end
+
+  # describe 'without an user login' do
+  #   context 'GET #index' do
+  #     let(:index_prototypes) { Prototype.page(params[:page]).per(Prototype::NUMBER_OF_DISPLAYED_PROTOTYPES) }
+
+  #     before :each do
+  #       get :index
+  #     end
+
+  #     it "assigns an array of requested prototypes to @prototypes" do
+  #       expect(assigns(:prototypes)).to eq index_prototypes
+  #     end
+
+  #     it "renders :index template" do
+  #       expect(response).to render_template :index
+  #     end
+  #   end
+
+  #   context 'GET #show' do
+  #     before :each do
+  #       get :show, params: { id: prototype.id }
+  #     end
+
+  #     it "assigns an the requested prototype to @prototype" do
+  #       expect(assigns(:prototype)).to eq prototype
+  #     end
+
+  #     it "assigns comments on the prototype to @comments" do
+  #       expect(assigns(:comments)).to eq prototype.comments
+  #     end
+
+  #     it "renders :show template" do
+  #       expect(response).to render_template :show
+  #     end
+  #   end
+
+  #   context 'GET #new' do
+  #     before :each do
+  #       get :new
+  #     end
+
+  #     it "redirect to new_user_session_path" do
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+
+  #   context 'POST #create' do
+  #     before :each do
+  #       post :create, params: params
+  #     end
+
+  #     it "redirect to new_user_session_path" do
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+
+  #   context 'GET #edit' do
+  #     before :each do
+  #       get :edit, params: { id: prototype.id }
+  #     end
+
+  #     it "redirect to new_user_session_path" do
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+
+  #   context 'PATCH #update' do
+  #     before :each do
+  #       patch :update, params: { id: prototype.id }
+  #     end
+  #     it "redirect to new_user_session_path" do
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+
+  #   context 'DELETE #destroy' do
+  #     before :each do
+  #       delete :destroy, params: { id: prototype.id }
+  #     end
+
+  #     it "redirect to new_user_session_path" do
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+  # end
+end
