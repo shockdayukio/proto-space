b643f7808267eb36e60b6044250b97c43c326ed7
yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-07 19:08:58 +0900

    Removed the factory files from github , which are not related to the test of the user model

--- spec/factories/comments.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    text { Faker::Hacker.say_something_smart }
-
-    association :user, factory: :user
-    association :prototype, factory: :prototype
-  end
-end
