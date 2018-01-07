30c934f37dd210e4100ad38cab65d7c5440f502c
yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-07 18:34:53 +0900

    Added validations on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  MINIMAM_NUMBER_OF_PASSWORD = 6
+  MAXIMAM_NUMBER_OF_PASSWORD = 128
+
   has_many :prototypes
   has_many :likes
   has_many :comments
@@ -6,4 +9,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+
+  validates :avatar, :profile, :works, :occupation, :password, presence: true
+  validates :name, :email, presence: true, uniqueness: true
+  validates :password, length: { in: MINIMAM_NUMBER_OF_PASSWORD..MAXIMAM_NUMBER_OF_PASSWORD }
 end
