2773e8448c38907eed22506d8f828597d5b1346b
yukihiro <yukihiro.ueda.1105@gmail.com>
2018-01-07 17:35:01 +0900

    Added validations on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+
+  validates :avatar, :profile, :works, :occupation, :password, presence: true
+  validates :name, :email, presence: true, uniqueness: true
+  validates :password, length: { in: 6..128 }
 end
